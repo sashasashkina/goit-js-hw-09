7e05237d67ac38c100c10930adc4730737687b5f
sashasashkina <koshyl.apps@gmail.com>
2023-11-12 14:08:55 +0200

    two

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -12,7 +12,7 @@ const secondsEl = document.querySelector('[data-seconds]');
 
 startBtn.setAttribute('disabled', '');
 
-let mareData = null;
+let newData = null;
 let timeR = null;
 
 const options = {
@@ -26,11 +26,61 @@ const options = {
       startBtn.setAttribute('disabled', '');
       dateInput.style.borderColor = 'red';
     } else {
-      chosenDate = selectedDates[0];
+      newData = selectedDates[0];
       startBtn.removeAttribute('disabled');
       startBtn.addEventListener('click', timerOn);
       dateInput.style.borderColor = '#569ff7';
     }
   },
 };
+
 flatpickr('#datetime-picker', options);
+
+function timerOn() {
+  timeR = setInterval(() => {
+    startBtn.setAttribute('disabled', '');
+    dateInput.setAttribute('disabled', '');
+
+    const currentTime = Date.now();
+    const deltaTime = newData - currentTime;
+
+    if (deltaTime < 1000) {
+      clearInterval(timeR);
+      startBtn.removeAttribute('disabled');
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
+    appClockInterface({ days, hours, minutes, seconds });
+  }, 1000);
+}
+
+function appClockInterface({ days, hours, minutes, seconds }) {
+  daysEl.textContent = days;
+  hoursEl.textContent = hours;
+  minutesEl.textContent = minutes;
+  secondsEl.textContent = seconds;
+}
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addStringZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addStringZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addStringZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addStringZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+function addStringZero(value) {
+  return String(value);
+  //   .padStart(2, '0');
+}
